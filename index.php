b4cf486d7013554c98999501470c17970d586bc9
Grzegorz Kowalski <grzegorz.adam.kowalski@outlook.com>
2020-08-06 16:58:05 +0200

    Basic security, require password, compare to sha256 hash

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <?php
 
+/* config section */
+
+// basic security, sha2565 hash of password to be used as 'pass' parameter
+$security_password_hash = '05fa2a6a4734b5efb4d7c4f33077fc1d2f1b9eff486b24a4e4e5953de50dfe89';
+
+// basic security, all paths MUST contain this
+$path_limit = '/var/www/html';
+
+/* end of config */
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-if (($_GET['pass'] ?? '') !='gk') die('Not authorized');
+if (hash('sha256', ($_GET['pass'] ?? '')) != $security_password_hash) die('Not authorized');
 
 $path = realpath($_REQUEST['path'] ?? '.');
-$path_limit = '/var/www/html';
 if (strpos($path, $path_limit) === false) die('Not authorized');
 
 if (is_dir($path) == false && isset($_POST['content'])) {
